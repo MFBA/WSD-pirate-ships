5530c68bb1b1934ba20d9ea83a48c5e393001de5
MFBA <muhammadfahad870@gmail.com>
2023-01-10 02:44:31 +0500

    sql final

--- user.sql
+++ user.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 09, 2023 at 05:50 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Host: localhost
+-- Generation Time: Jan 09, 2023 at 10:43 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,16 +32,8 @@ CREATE TABLE `user` (
   `password` varchar(128) NOT NULL,
   `team` varchar(128) NOT NULL,
   `shipcell` int(128) NOT NULL,
-  `Online` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`username`, `password`, `team`, `shipcell`, `Online`) VALUES
-('umairrkhn', 'f4e5cb46c14e342ea1b248dc9cd137c5df0a185c9c82ef3a6eeb990c87939f51', 'Red', 44, 1),
-('ata potta', 'b687731c02296e354ac6be4a24fb642aeb49a56f07ed81a099b3086a9ee9c7ae', 'Green', 84, 1);
+  `Online` int(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
